802fa45826e8e3efd7f41e57db95b93a2b6ccb6d
Mustajab <Mustajab.Akhtar@makemytrip.com>
2017-03-09 20:03:07 +0530

    Initial commit

---
+++ src/main/java/com/mustajab/api/world/main/App.java
@@ -0,0 +1,13 @@
+package com.mustajab.api.world.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
